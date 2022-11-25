93918cff9e857ee766004155e7078283b3dda86c
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-25 14:23:33 +0100

    primo punto

---
+++ csharp-geometria/Class1.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+Rettangolo rettangolo1 = new Rettangolo();
+rettangolo1.AltezzaRettangolo = 12;
+rettangolo1.BaseRettangolo = 7;
+rettangolo1.PerimetroRettangolo = 2 * rettangolo1.AltezzaRettangolo + 2 * rettangolo1.BaseRettangolo;
+rettangolo1.AreaRettangolo = rettangolo1.BaseRettangolo * rettangolo1.AltezzaRettangolo;
+
+
+Console.WriteLine(rettangolo1.AltezzaRettangolo);
+Console.WriteLine(rettangolo1.BaseRettangolo);
+Console.WriteLine(rettangolo1.PerimetroRettangolo);
+Console.WriteLine(rettangolo1.AreaRettangolo);
+
+
