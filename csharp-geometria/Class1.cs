8786a05b90f60e6a3643f2e77e4f5e6fbbbbbecf
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-25 15:21:33 +0100

    Non prende il secondo readline

--- csharp-geometria/Class1.cs
+++ csharp-geometria/Class1.cs
@@ -1,17 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Class1 {
 
+    public class Rettangolo
+    {
+        public int Baserettangolo;
+        public int AltezzaRettangolo;
+        public int PerimetroRettangolo;
+        public int AreaRettangolo;
+    }
 
-Rettangolo rettangolo1 = new Rettangolo();
-rettangolo1.AltezzaRettangolo = 12;
-rettangolo1.BaseRettangolo = 7;
-rettangolo1.PerimetroRettangolo = 2 * rettangolo1.AltezzaRettangolo + 2 * rettangolo1.BaseRettangolo;
-rettangolo1.AreaRettangolo = rettangolo1.BaseRettangolo * rettangolo1.AltezzaRettangolo;
-
-
-Console.WriteLine(rettangolo1.AltezzaRettangolo);
-Console.WriteLine(rettangolo1.BaseRettangolo);
-Console.WriteLine(rettangolo1.PerimetroRettangolo);
-Console.WriteLine(rettangolo1.AreaRettangolo);
-
-
+}
--- csharp-geometria/Program.cs
+++ csharp-geometria/Program.cs
@@ -1,9 +1,37 @@
 ﻿
-public class Rettangolo
-{
-    public int BaseRettangolo;
-    public int AltezzaRettangolo;
-    public int PerimetroRettangolo;
-    public int AreaRettangolo;
+using Class1;
 
-}
+
+
+Rettangolo rettangolo1 = new Rettangolo();
+Rettangolo rettangoloutente = new Rettangolo();
+
+rettangolo1.Baserettangolo = 20;
+rettangolo1.AreaRettangolo = 10;
+rettangolo1.PerimetroRettangolo = 2 * rettangolo1.Baserettangolo + 2 * rettangolo1.AltezzaRettangolo;
+rettangolo1.AreaRettangolo = rettangolo1.Baserettangolo * rettangolo1.AreaRettangolo;
+
+
+Console.WriteLine("--Rettangolobase--");
+Console.WriteLine("base: " + rettangolo1.Baserettangolo + " cm");
+Console.WriteLine("altezza: " + rettangolo1.AltezzaRettangolo + " cm");
+Console.WriteLine("perimetro:" + rettangolo1.PerimetroRettangolo + " cm");
+Console.WriteLine("area: " + rettangolo1.AreaRettangolo + " cm2");
+
+Console.WriteLine("Inserire Base Rettangolo: ");
+int baseutente = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserire Altezza Rettangolo: ");
+int altezzautente = int.Parse(Console.ReadLine());  
+
+rettangoloutente.Baserettangolo = baseutente;
+rettangoloutente.AreaRettangolo = altezzautente;
+rettangoloutente.PerimetroRettangolo = 2 * rettangoloutente.Baserettangolo + 2 * rettangoloutente.AltezzaRettangolo;
+rettangoloutente.AreaRettangolo = rettangoloutente.Baserettangolo * rettangoloutente.AltezzaRettangolo;
+
+
+Console.WriteLine("--Rettangoutente--");
+Console.WriteLine("base: " + rettangoloutente.Baserettangolo + " cm");
+Console.WriteLine("altezza: " + rettangoloutente.AltezzaRettangolo + " cm");
+Console.WriteLine("perimetro:" + rettangoloutente.PerimetroRettangolo + " cm");
+Console.WriteLine("area: " + rettangoloutente.AreaRettangolo + " cm2");
